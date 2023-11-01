56977d429379f8371bf77a3868bc12debb5012ae
rayrobi <136340536+rayrobi@users.noreply.github.com>
2023-11-01 11:08:17 -0400

    Add files via upload

---
+++ Ex02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Servidor</title>
+</head>
+<body>
+    <h1>
+    <?php 
+
+    echo "Olá, Mundo!\u{1F344}";
+   
+    phpinfo();
+    ?>
+    </h1>
+</body>
+</html>
---
+++ Ex03/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    $campus="Instituto Federal";
+    echo "<p>Eu estudo no $campus!</p>";
+    ?>
+</body>
+</html>
